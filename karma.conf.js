1413ba0c4e2e5f994ad0adad1caa71a21b3416b0
Vladimir Kulikov <vladimir_kulikov@epam.com>
2021-04-09 13:04:22 +0300

    update flag, set chrome bin path

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,10 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  if(config.browsers.includes('ChromeHeadlessCI')) {
+    process.env.CHROME_BIN = require('puppeteer').executablePath();
+  }
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
